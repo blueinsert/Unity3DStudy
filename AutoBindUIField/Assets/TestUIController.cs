5d3b4857c9e76224b3ef85d44588bf11387b939f
bluebean <1401597377@qq.com>
2019-01-06 21:29:11 +0800

    利用自定义Attribute自动绑定ui组件

---
+++ AutoBindUIField/Assets/TestUIController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestUIController : MonoBehaviour {
+    [AutoBind("ButtonLoad")]
+    public Button btnLoad;
+    [AutoBind("ButtonNew")]
+    public Button btnNew;
+    [AutoBind("ButtonSetting")]
+    public Button btnSetting;
+
+}
---
+++ AutoBindUIField/Assets/UIIntent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AutoBindAttribute : Attribute
+{
+    public string path { get; private set; }
+
+    public AutoBindAttribute(string path)
+    {
+        this.path = path;
+    }
+}
+
+public class UIIntent {
+
+    public string name;
+    public string prefabName;
+    public string uiControllerName;
+
+}
