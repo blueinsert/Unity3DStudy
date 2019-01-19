5f2b204a49538dbdbe5aff0b1437b7d08b303e94
bluebean <1401597377@qq.com>
2019-01-19 21:02:03 +0800

    实现协程调度

---
+++ TinyCoroutineManager/Assets/Test.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using bluebean.utils;
+using WaitForSeconds = bluebean.utils.WaitForSeconds;
+
+public class Test : MonoBehaviour {
+
+    TinyCorcoutineManager corcoutineManager = new TinyCorcoutineManager();
+
+	// Use this for initialization
+	void Start () {
+        corcoutineManager.StartCorcoutine(Test1());
+        //StartCoroutine(Test1());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        corcoutineManager.Update();
+	}
+
+    //test case for TinyCorcoutineManager
+    IEnumerator Test1()
+    {
+        print("WaitForFames start:" + Time.frameCount);
+        yield return new WaitForFrames(15);
+        print("WaitForFames end:" + Time.frameCount);
+        print("WaitForSeconds start" + Time.time);
+        yield return new WaitForSeconds(2);
+        print("WaitForSeconds end" + Time.time);
+        yield return Child();
+        print(Time.frameCount);
+        yield return new WaitForNextFrame();
+        print(Time.frameCount);
+        yield return new WaitForNextFrame();
+        print(Time.frameCount);
+        yield return new Break();
+    }
+
+    IEnumerator Child()
+    {
+        print("in child, " + Time.frameCount);
+        yield return null;
+        print("in child, " + Time.frameCount);
+        yield return ChildChild();
+        yield return new WaitForNextFrame();
+        print("in child, " + Time.frameCount);
+    }
+
+
+    IEnumerator ChildChild()
+    {
+        print("in child'child, " + Time.frameCount);
+        yield return null;
+        print("in child'child, " + Time.frameCount);
+        yield return new WaitForNextFrame();
+        print("in child'child, 提前退出, " + Time.frameCount);
+        yield return new Break();
+        print("others be skipped");
+    }
+
+}
---
+++ TinyCoroutineManager/Assets/TinyCorcoutineManager.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace bluebean.utils
+{
+    public class YieldInstruction
+    {
+        public virtual void Update() { }
+        public virtual bool IsPassed() { return true; }
+    }
+    public class WaitForNextFrame : YieldInstruction
+    {
+
+    }
+
+    public class Break : YieldInstruction
+    {
+
+    }
+
+    public class WaitForSeconds : YieldInstruction
+    {
+        private float m_duration;
+
+        public WaitForSeconds(float duration)
+        {
+            m_duration = duration;
+        }
+
+        public override void Update()
+        {
+            m_duration -= Time.deltaTime;
+        }
+
+        public override bool IsPassed()
+        {
+            return m_duration <= 0;
+        }
+    }
+
+    public class WaitForFrames: YieldInstruction
+    {
+        private int m_frames;
+
+        public WaitForFrames(int frames)
+        {
+            m_frames = frames;
+        }
+
+        public override void Update()
+        {
+            m_frames--;
+        }
+
+        public override bool IsPassed()
+        {
+            return m_frames <= 0;
+        }
+    }
+
+    public class TinyCorcoutineManager
+    {
+        private LinkedList<IEnumerator> corcoutines = new LinkedList<IEnumerator>();
+        private List<IEnumerator> deadCorcoutines = new List<IEnumerator>();
+
+        public void Update()
+        {
+            var first = corcoutines.First;
+            var node = first;
+            while (node != null)
+            { 
+                if (!MoveNext(node.Value))
+                {
+                    deadCorcoutines.Add(node.Value);
+                }
+                node = node.Next;
+            }
+            if (deadCorcoutines.Count != 0)
+            {
+                foreach (var r in deadCorcoutines)
+                {
+                    corcoutines.Remove(r);
+                }
+                deadCorcoutines.Clear();
+            }
+        }
+
+        private bool MoveNext(IEnumerator corcoutine)
+        {
+            var current = corcoutine.Current; 
+            if (current is IEnumerator && MoveNext(corcoutine.Current as IEnumerator)) //优先执行子协程
+            {
+                return true;
+            }else if(current is YieldInstruction)
+            {
+                if(current is Break)
+                {
+                    return false;
+                }else
+                {
+                    var yieldInstruction = current as YieldInstruction;
+                    yieldInstruction.Update();
+                    if (yieldInstruction.IsPassed())
+                    {
+                        return corcoutine.MoveNext();
+                    }
+                    return true;
+                }  
+            }
+            else
+            {
+                return corcoutine.MoveNext();
+            }
+        }
+
+        public void StartCorcoutine(IEnumerator corcoutine)
+        {
+            corcoutines.AddLast(corcoutine);
+        }
+
+    }
+}
