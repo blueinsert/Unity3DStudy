d47d92097c8356b0f5020f8f60f4a34ce75335a0
bluebean <1401597377@qq.com>
2020-04-25 23:38:45 +0800

    new

---
+++ 2.5DBox/Assets/TwoPointFiveDimensionAAQuadEditor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TwoPointFiveDimensionAAQuad))]
+public class TwoPointFiveDimensionAAQuadEditor : Editor
+{
+    TwoPointFiveDimensionAAQuad m_target;
+
+    private void OnEnable()
+    {
+        m_target = target as TwoPointFiveDimensionAAQuad;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        m_target.m_axis = (TwoPointFiveDimensionAAQuadAxis)EditorGUILayout.EnumPopup("Axis",m_target.m_axis);
+        switch (m_target.m_axis)
+        {
+            case TwoPointFiveDimensionAAQuadAxis.x:
+                m_target.yz_plane = EditorGUILayout.FloatField("x", m_target.yz_plane);
+                m_target.m_min.x = m_target.yz_plane;
+                m_target.m_max.x = m_target.yz_plane;
+                break;
+            case TwoPointFiveDimensionAAQuadAxis.y:
+                m_target.xz_plane = EditorGUILayout.FloatField("y", m_target.xz_plane);
+                m_target.m_min.y = m_target.xz_plane;
+                m_target.m_max.y = m_target.xz_plane;
+                break;
+            case TwoPointFiveDimensionAAQuadAxis.z:
+                m_target.xy_plane = EditorGUILayout.FloatField("z", m_target.xy_plane);
+                m_target.m_min.z = m_target.xy_plane;
+                m_target.m_max.z = m_target.xy_plane;
+                break;
+        }
+        m_target.m_min = EditorGUILayout.Vector3Field("min", m_target.m_min);
+        m_target.m_max = EditorGUILayout.Vector3Field("max", m_target.m_max);
+        
+        m_target.m_localCoord = (TwoPointFiveDimensionCoordianteSystem)EditorGUILayout.ObjectField("LocalCoord", m_target.m_localCoord, typeof(TwoPointFiveDimensionCoordianteSystem));
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+    }
+}
---
+++ 2.5DBox/Assets/TwoPointFiveDimensionCoordianteSystem.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class TwoPointFiveDimensionCoordianteSystem : MonoBehaviour
+{
+
+    public Vector2 m_origin = Vector2.zero;
+    public float m_angle = 0;
+    public const float K = 0.7f; 
+
+    public Vector3 Pos3DTo2D(Vector3 pos3d)
+    {
+        var sin = Mathf.Sin(m_angle * Mathf.Deg2Rad);
+        var cos = Mathf.Cos(m_angle * Mathf.Deg2Rad);
+        float x = pos3d.x + pos3d.y * K * cos;
+        float y = pos3d.z + pos3d.y * K * sin;
+        return new Vector3(x, y, 0);
+    }
+
+    public static Vector2 Rotate(Vector2 u, float angle)
+    {
+        float x, y;
+        x = u.x * Mathf.Cos(Mathf.Deg2Rad * angle) - u.y * Mathf.Sin(Mathf.Deg2Rad * angle);
+        y = u.x * Mathf.Sin(Mathf.Deg2Rad * angle) + u.y * Mathf.Cos(Mathf.Deg2Rad * angle);
+        return new Vector3(x, y, 0);
+    }
+
+    public static void DrawLine(Vector2 p1, Vector2 p2, Color color)
+    {
+        Gizmos.color = color;
+        Gizmos.DrawLine(p1, p2); 
+    }
+
+    public static void DrawSingleArrowLine(Vector2 start, Vector2 end, float arrowLength, Color color)
+    {
+        DrawLine(start, end, color);
+        var n = (start - end).normalized;
+        var dir1 = Rotate(n,30);
+        var dir2 = Rotate(n,-30);
+        var p1 = end + dir1 * arrowLength;
+        var p2 = end + dir2 * arrowLength;
+        DrawLine(p1, end, color);
+        DrawLine(p2, end, color);
+    }
+
+    public void DrawCoordianteSystem()
+    {
+        float scale = HandleUtility.GetHandleSize(this.transform.position);
+        float length = scale * 0.8f;
+        DrawSingleArrowLine(Vector2.zero, new Vector2(length, 0),length*0.3f, Color.red);
+        var sin = Mathf.Sin(m_angle * Mathf.Deg2Rad);
+        var cos = Mathf.Cos(m_angle * Mathf.Deg2Rad);
+        DrawSingleArrowLine(Vector2.zero, new Vector2(length*cos, length*sin), length * 0.3f, Color.green);
+        DrawSingleArrowLine(Vector2.zero, new Vector2(0, length), length * 0.3f, Color.blue);
+    }
+
+    public void OnDrawGizmos()
+    {
+        //Gizmos.matrix = this.transform.localToWorldMatrix * Matrix4x4.Translate(new Vector3(m_origin.x, m_origin.y, 0));
+        //DrawCoordianteSystem();
+    }
+
+}
