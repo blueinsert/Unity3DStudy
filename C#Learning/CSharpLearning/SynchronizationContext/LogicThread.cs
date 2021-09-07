62b3dafb5120b3cff196706964daf44e50bb2941
zhongzhengxiang <zhongzhengxiang@blackjack-inc.com>
2021-09-07 15:48:59 +0800

    new

---
+++ C#Learning/CSharpLearning/SynchronizationContext/LogicThread.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace bluebean
+{
+    public class LogicThread
+    {
+        private Thread m_thread;
+        private CancellationTokenSource m_cancelSource = null;
+        private CancellationToken m_cancelToken;
+        private TaskScheduler m_taskScheduler;
+        protected TickBaseSynchronizationContext m_syncCtx;
+
+        public void Start()
+        {
+            // 首先设置SynchronizationContext和TaskScheduler
+            m_syncCtx = new TickBaseSynchronizationContext();
+            SynchronizationContext.SetSynchronizationContext(m_syncCtx);
+            m_taskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+            // 初始化cancel现场
+            m_cancelSource = new CancellationTokenSource();
+            m_cancelToken = m_cancelSource.Token;
+            System.Console.WriteLine(string.Format("LogicThread:Start thread:{0}", Thread.CurrentThread.ManagedThreadId));
+
+            Task.Factory.StartNew(ManagedThreadProc, "managedTask1", m_cancelToken, TaskCreationOptions.LongRunning, m_taskScheduler);
+            Task.Factory.StartNew(ManagedThreadProc, "managedTask2", m_cancelToken, TaskCreationOptions.LongRunning, m_taskScheduler);
+
+            m_thread = new Thread(MainThreadProc);
+            m_thread.Start();
+        }
+
+        /// <summary>
+        /// 线程函数
+        /// </summary>
+        /// <param name="obj"></param>
+        protected async void ManagedThreadProc(Object state)
+        {
+            while (!m_cancelToken.IsCancellationRequested)
+            {
+                System.Console.WriteLine(string.Format("ManagedThreadProc {0} thread:{1}", state, Thread.CurrentThread.ManagedThreadId));
+                Thread.Sleep(10);
+                await Task.Yield();
+            }
+        }
+
+        /// <summary>
+        /// 线程函数
+        /// </summary>
+        /// <param name="obj"></param>
+        protected void MainThreadProc(object obj)
+        {
+
+            while (!m_cancelToken.IsCancellationRequested)
+            {
+                System.Console.WriteLine(string.Format("MainThreadProc Tick threadId: {0}",Thread.CurrentThread.ManagedThreadId));
+                m_syncCtx.Tick();
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Stop()
+        {
+            m_cancelSource.Cancel();
+            // 调用dispose，释放资源
+            m_cancelSource.Dispose();
+        }
+    }
+}
---
+++ C#Learning/CSharpLearning/SynchronizationContext/TickBaseSynchronizationContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace bluebean
+{
+    public class TickBaseSynchronizationContext : SynchronizationContext
+    {
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            var task = state as Task;
+            System.Console.WriteLine(string.Format("{0} Post thread:{1} taskName:{2}", "TickBaseSynchronizationContext", Thread.CurrentThread.ManagedThreadId, task.AsyncState));
+            m_taskQueue.Enqueue(new KeyValuePair<SendOrPostCallback, object>(d, state));
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            System.Console.WriteLine(string.Format("{0} Send thread:{1} cb:{2}", "TickBaseSynchronizationContext", Thread.CurrentThread.ManagedThreadId, 0));
+            m_taskQueue.Enqueue(new KeyValuePair<SendOrPostCallback, object>(d, state));
+        }
+
+        /// <summary>
+        /// 驱动task调度
+        /// </summary>
+        /// <returns></returns>
+        public bool Tick()
+        {
+            KeyValuePair<SendOrPostCallback, object> item;
+            if (m_taskQueue.TryDequeue(out item))
+            {
+                try
+                {
+                    item.Key(item.Value);
+                }
+                catch (Exception e)
+                {
+                    if (EventOnException != null)
+                    {
+                        EventOnException(e);
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 通知使用者调度的task抛出异常
+        /// </summary>
+        public event Action<Exception> EventOnException;
+
+        /// <summary>
+        /// 调度队列
+        /// </summary>
+        private ConcurrentQueue<KeyValuePair<SendOrPostCallback, object>> m_taskQueue = new ConcurrentQueue<KeyValuePair<SendOrPostCallback, object>>();
+    }
+}
