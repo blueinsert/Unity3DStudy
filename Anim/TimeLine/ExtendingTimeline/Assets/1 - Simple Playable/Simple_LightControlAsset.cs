a295e7ade2070840289f7decd87321f9a9ce73fd
bluebean <1401597377@qq.com>
2024-12-02 14:06:06 +0800

    new

---
+++ Anim/TimeLine/ExtendingTimeline/Assets/1 - Simple Playable/Simple_LightControlAsset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Simple
+{
+	public class Simple_LightControlAsset : PlayableAsset
+	{
+		public ExposedReference<Light> light;
+		public Color color = Color.white;
+		public float intensity = 1f;
+
+		public override Playable CreatePlayable (PlayableGraph graph, GameObject owner)
+		{
+			var playable = ScriptPlayable<Simple_LightControlBehaviour>.Create(graph);
+
+			var behaviour = playable.GetBehaviour();
+			behaviour.light = light.Resolve(graph.GetResolver());
+			behaviour.color = color;
+			behaviour.intensity = intensity;
+
+			return playable;
+		}
+	}
+}
---
+++ Anim/TimeLine/ExtendingTimeline/Assets/3 - MixerBehaviour/Mixer_LightControlAsset.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace MixerExample
+{
+	public class Mixer_LightControlAsset : PlayableAsset, ITimelineClipAsset
+	{
+		public Color color = Color.white;
+		public float intensity = 1f;
+
+		public ClipCaps clipCaps
+		{
+			get { return ClipCaps.Blending; }
+		}
+
+		public override Playable CreatePlayable (PlayableGraph graph, GameObject owner)
+		{
+			var playable = ScriptPlayable<Mixer_LightControlBehaviour>.Create(graph);
+			
+			var lightControlBehaviour = playable.GetBehaviour();
+			lightControlBehaviour.color = color;
+			lightControlBehaviour.intensity = intensity;
+
+			return playable;   
+		}
+	}
+}
